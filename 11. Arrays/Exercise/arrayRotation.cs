8a10b58058e51ae858eae4c8a6cc01b7f93c214c
Hristo Papanov <105742663+Dgeto1@users.noreply.github.com>
2022-06-05 20:52:31 +0300

    created array rotation

---
+++ 11. Arrays/Exercise/arrayRotation.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+
+namespace dayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            for(int i=0; i<n; i++)
+            {
+                int firstEl = arr[0];
+                for(int j=0; j<arr.Length-1;j++)
+                {
+                    arr[j] = arr[j + 1];
+                }
+                arr[arr.Length - 1] = firstEl;
+            }
+            Console.WriteLine(String.Join(" ", arr));
+
+        }
+    }
+}
+
