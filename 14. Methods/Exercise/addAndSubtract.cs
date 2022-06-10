7d59aaaf256a6101ef88322af7f69739fea2d81d
Hristo Papanov <105742663+Dgeto1@users.noreply.github.com>
2022-06-10 20:02:11 +0300

    created addAndSubtract.cs

---
+++ 14. Methods/Exercise/addAndSubtract.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace addAndSubstract
+{
+    class Program
+    {
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Subtract(int num1, int num2, int num3)
+        {
+            return (Add(num1, num2) - num3);
+        }
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Subtract(num1,num2,num3));
+        }
+    }
+}
+
