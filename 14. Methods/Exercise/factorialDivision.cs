1314d1b97f751577906232c355cb560348459a06
Hristo Papanov <105742663+Dgeto1@users.noreply.github.com>
2022-06-13 19:08:04 +0300

    uodated.

--- 14. Methods/Exercise/factorialDivision.cs
+++ 14. Methods/Exercise/factorialDivision.cs
@@ -4,7 +4,7 @@ namespace factorialDivision
 {
     class Program
     {
-        static int FactorialFirstNum(int num1)
+        static decimal FactorialFirstNum(decimal num1)
         {
             if(num1==1)
             {
@@ -12,7 +12,7 @@ namespace factorialDivision
             }
             return num1* FactorialFirstNum(num1-1);
         }
-        static int FactorialSecondNum(int num2)
+        static decimal FactorialSecondNum(decimal num2)
         {
             if (num2 == 1)
             {
@@ -20,16 +20,17 @@ namespace factorialDivision
             }
             return num2 * FactorialSecondNum(num2 - 1);
         }
-        static string Division(int num1,int num2)
+        static string Division(decimal num1,decimal num2)
         {
-            return $"{FactorialFirstNum(num1) / FactorialSecondNum(num2):f2}";
+            return (FactorialFirstNum(num1) / FactorialSecondNum(num2)).ToString("f2");
         }
         static void Main(string[] args)
         {
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
+            decimal num1 = decimal.Parse(Console.ReadLine());
+            decimal num2 = decimal.Parse(Console.ReadLine());
             Console.WriteLine(Division(num1,num2));
         }
     }
 }
+
 
