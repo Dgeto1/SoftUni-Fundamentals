ee4219750fe97845ebe05bae607111446f3c9376
Hristo Papanov <105742663+Dgeto1@users.noreply.github.com>
2022-06-19 14:06:07 +0300

    created 05. removeNegativesAndReverse.cs

---
+++ 17. List/Lab/05. removeNegativesAndReverse.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace removeAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            for(int i=0; i<numbers.Count;i++)
+            {
+                if (numbers[i]<0)
+                {
+                    numbers.Remove(numbers[i]);
+                    i--;
+                } 
+            }
+            if(numbers.Count==0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                numbers.Reverse();
+                Console.WriteLine(String.Join(" ", numbers));
+            }
+        }
+    }
+}
+
