242cf96d9a6323a2e335fd01122ef065fc243626
Hristo Papanov <hristopapanov13@gmail.com>
2022-06-25 21:44:30 +0300

    Published before exam exercises

---
+++ MidExamPreparation/02_ArrayModifier/02_ArrayModifier/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _02_ArrayModifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command;
+            while((command=Console.ReadLine())!="end")
+            {
+                string[] segmnets = command.Split();
+                if (segmnets[0]=="swap")
+                {
+                    int index1 = int.Parse(segmnets[1]);
+                    int index2 = int.Parse(segmnets[2]);
+                    int a = numbers[index1];
+                    numbers[index1] = numbers[index2];
+                    numbers[index2] = a;
+                }
+                else if (segmnets[0]=="multiply")
+                {
+                    int index1 = int.Parse(segmnets[1]);
+                    int index2 = int.Parse(segmnets[2]);
+                    numbers[index1] *= numbers[index2];
+                }
+                else if (segmnets[0]=="decrease")
+                {
+                    for(int i=0;i<numbers.Length;i++)
+                    {
+                        numbers[i]--;
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
+
---
+++ MidExamPreparation/03_Numbers/03_Numbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int average = 0;
+            List<int> greaterNumbers = new List<int>();
+            for(int i=0; i<numbers.Count;i++)
+            {
+                average += numbers[i];
+            }
+            average /= numbers.Count;
+            int counter = 0;
+            numbers.Sort();
+            numbers.Reverse();
+            for(int i=0; i<numbers.Count;i++)
+            {
+                if (numbers[i]>average)
+                {
+                    counter++;
+                    greaterNumbers.Add(numbers[i]);
+                }
+                if(counter==5)
+                {
+                    break;
+                }
+            }
+            if(greaterNumbers.Count==0)
+            {
+                Console.WriteLine("No");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", greaterNumbers));
+            }
+        }
+    }
+}
+
