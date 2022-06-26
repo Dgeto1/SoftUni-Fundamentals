e04496ec0b82a503616adc22ef82ccd3a3864d87
Hristo Papanov <hristopapanov13@gmail.com>
2022-06-26 13:36:08 +0300

    added all mid exam tasks

---
+++ Mid Exam/01_TheBiscuitFactory/01_TheBiscuitFactory/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01_TheBiscuitFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberBiscuits = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+            int numberBiscuitsSecondFactory = int.Parse(Console.ReadLine());
+            double producedBiscuits = 0;
+            for(int i=1; i<=30;i++)
+            {
+                if(i%3==0)
+                {
+                    double prodBisc = numberBiscuits * workers;
+                    prodBisc *= 0.75;
+                    producedBiscuits += Math.Floor(prodBisc); 
+                }
+                else
+                {
+                    producedBiscuits += numberBiscuits * workers;
+                }
+            }
+            Console.WriteLine($"You have produced {producedBiscuits} biscuits for the past month.");
+            if(producedBiscuits>numberBiscuitsSecondFactory)
+            {
+                double more = producedBiscuits - numberBiscuitsSecondFactory;
+                Console.WriteLine($"You produce {more/numberBiscuitsSecondFactory*100:f2} percent more biscuits.");
+            }
+            else
+            {
+                double less = numberBiscuitsSecondFactory-producedBiscuits;
+                Console.WriteLine($"You produce {less /numberBiscuitsSecondFactory * 100:f2} percent less biscuits.");
+            }
+        }
+    }
+}
+
---
+++ Mid Exam/03_ChatLogger/03_ChatLogger/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03_ChatLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chats = new List<string>();
+            string command;
+            while((command=Console.ReadLine())!="end")
+            {
+                string[] segments = command.Split();
+                if (segments[0]=="Chat")
+                {
+                    string message = segments[1];
+                    chats.Add(message);
+                }
+                else if (segments[0]=="Delete")
+                {
+                    string message = segments[1];
+                    if(chats.Contains(message))
+                    {
+                        chats.Remove(message);
+                    }
+                }
+                else if (segments[0]=="Edit")
+                {
+                    string message = segments[1];
+                    string editedVersion = segments[2];
+                    for(int i=0; i<chats.Count;i++)
+                    {
+                        if (chats[i]==message)
+                        {
+                            chats[i] = editedVersion;
+                        }
+                    }
+                }
+                else if (segments[0]=="Pin")
+                {
+                    string message = segments[1];
+                    if(chats.Contains(message))
+                    {
+                        chats.Remove(message);
+                        chats.Add(message);
+                    }
+                }
+                else if (segments[0]=="Spam")
+                {
+                    for(int i=1; i < segments.Length;i++)
+                    {
+                        chats.Add(segments[i]);
+                    }
+                }
+            }
+            foreach(string x in chats)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
+
