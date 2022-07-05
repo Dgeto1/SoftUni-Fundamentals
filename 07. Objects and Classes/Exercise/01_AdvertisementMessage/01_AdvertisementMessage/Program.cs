8268f78755f3476c468375c55d49109d9021fccb
Hristo Papanov <hristopapanov13@gmail.com>
2022-07-05 16:52:22 +0300

    published ex

---
+++ 07. Objects and Classes/Exercise/01_AdvertisementMessage/01_AdvertisementMessage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01_AdvertisementMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phrases = { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+            string[] events = { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now I feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+            string[] authors = { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+            string[] cities = { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+            int count = int.Parse(Console.ReadLine());
+            Random r = new Random();
+            for(int i=1; i<=count; i++)
+            {
+                int indexPhrase = r.Next(0, phrases.Length);
+                string phrase = phrases[indexPhrase];
+                int indexEvents = r.Next(0, events.Length);
+                string even = events[indexEvents];
+                int indexAuthor = r.Next(0, authors.Length);
+                string author = authors[indexAuthor];
+                int indexCity = r.Next(0, cities.Length);
+                string city = cities[indexCity];
+                Console.WriteLine($"{phrase} {even} {author} - {city}.");
+
+            }
+        }
+    }
+}
+
---
+++ 07. Objects and Classes/Exercise/06_VehicleCatalogue/06_VehicleCatalogue/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_VehicleCatalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Vehicle> vehicles = new List<Vehicle>();
+            string command;
+            while((command = Console.ReadLine())!="End")
+            {
+                string[] segments = command.Split();
+                Vehicle vehicle = new Vehicle(segments[0], segments[1], segments[2], int.Parse(segments[3]));
+                vehicles.Add(vehicle);
+            }
+            string secondCommand;
+            while((secondCommand=Console.ReadLine())!="Close the Catalogue")
+            {
+                string carBrand = secondCommand;
+                foreach(Vehicle x in vehicles)
+                {
+                    if(carBrand==x.Model)
+                    {
+                        Console.WriteLine(x.Print());
+                    }
+                }
+            }
+            double averageHPCars = 0, averageHPTrucks = 0;
+            int countCars = 0, countTrucks = 0; ;
+            foreach(Vehicle x in vehicles)
+            {
+                if(x.Type=="car")
+                {
+                    averageHPCars += x.Horsepower;
+                    countCars++;
+                }
+                else
+                {
+                    averageHPTrucks += x.Horsepower;
+                    countTrucks++;
+                }
+                if(x.Type=="truck")
+                {
+                    averageHPTrucks += x.Horsepower;
+                    countTrucks++;
+                }
+            }
+            averageHPCars /= countCars;
+            averageHPTrucks /= countTrucks;
+            Console.WriteLine($"Cars have average horsepower of: {averageHPCars:f2}.");
+            Console.WriteLine($"Trucks have average horsepower of: {averageHPTrucks:f2}.");
+        }
+    }
+    class Vehicle
+    {
+        public string Type { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Horsepower { get; set; }
+        public Vehicle(string type, string model, string color, int horsepower)
+        {
+            this.Type = type;
+            this.Model = model;
+            this.Color = color;
+            this.Horsepower = horsepower;
+        }
+        public string Print()
+        {
+            if(this.Type=="car")
+            {
+                return $"Type: Car\nModel: {this.Model}\nColor: {this.Color}\nHorsepower: {this.Horsepower}";
+            }
+            else
+            {
+                return $"Type: Truck\nModel: {this.Model}\nColor: {this.Color}\nHorsepower: {this.Horsepower}";
+            }
+            
+        }
+    }
+}
+
