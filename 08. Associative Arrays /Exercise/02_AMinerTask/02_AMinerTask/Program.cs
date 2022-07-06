e08edc571549b4292bf65f13e9de592d331e246d
Hristo Papanov <hristopapanov13@gmail.com>
2022-07-06 20:57:55 +0300

    addet tasks

---
+++ 08. Associative Arrays /Exercise/02_AMinerTask/02_AMinerTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02_AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+            string command;
+            while((command=Console.ReadLine())!="stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if(resources.ContainsKey(command))
+                {
+                    resources[command] += quantity; 
+                }
+                else
+                {
+                    resources.Add(command, quantity);
+                }
+            }
+            foreach(var x in resources)
+            {
+                Console.WriteLine($"{x.Key} -> {x.Value}");
+            }
+        }
+    }
+}
+
