4b40c840f9f43c401274f91b0c1ccb01dde414eb
Hristo Papanov <hristopapanov13@gmail.com>
2022-07-06 20:56:36 +0300

    added two tasks

---
+++ 08. Associative Arrays /Exercise/01_CountCharsInStrings/01_CountCharsInStrings/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_CountCharsInStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<char> letters = Console.ReadLine().ToList();
+            for(int i=0; i<letters.Count;i++)
+            {
+                if (letters[i]==' ')
+                {
+                    letters.Remove(letters[i]);
+                }
+            }
+            
+            Dictionary<char, int> countLetters = new Dictionary<char, int>();
+            foreach(var x in letters)
+            {
+                if(countLetters.ContainsKey(x))
+                {
+                    countLetters[x]++;
+                }
+                else
+                {
+                    countLetters[x] = 1;
+                }
+            }
+            foreach(var x in countLetters)
+            {
+                Console.WriteLine($"{x.Key} -> {x.Value}");
+            }
+        }
+    }
+}
+
