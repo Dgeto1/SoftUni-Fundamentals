fec6d78e02fa8d35d2e596f235b6bbdc90e7ef2e
Hristo Papanov <hristopapanov13@gmail.com>
2022-07-06 20:20:33 +0300

    all lab tasks

---
+++ 08. Associative Arrays /Lab/01_CountRealNumbers/01_CountRealNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            SortedDictionary<double, int> sortedNumbersWithCount = new SortedDictionary<double, int>();
+            foreach(var number in numbers)
+            {
+                if(sortedNumbersWithCount.ContainsKey(number))
+                {
+                    sortedNumbersWithCount[number]++;
+                }
+                else
+                {
+                    sortedNumbersWithCount.Add(number, 1);
+                }
+            }
+            foreach(var nums in sortedNumbersWithCount)
+            {
+                Console.WriteLine($"{nums.Key} -> {nums.Value}");
+            }
+        }
+    }
+}
+
