fa17beb0d7b089be0ef414b9c85572b6fa26e5e9
Hristo Papanov <hristopapanov13@gmail.com>
2022-07-07 23:31:59 +0300

    added two more tasks

---
+++ 08. Associative Arrays /Exercise/04_Orders/04_Orders/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04_Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> productsPrices = new Dictionary<string, double>();
+            Dictionary<string, int> productsQuantity = new Dictionary<string, int>();
+            Dictionary<string, double> productsFinalPrices = new Dictionary<string, double>();
+            string command;
+            while((command=Console.ReadLine())!="buy")
+            {
+                string[] segments = command.Split();
+                string name = segments[0];
+                double price = double.Parse(segments[1]);
+                int quantity = int.Parse(segments[2]);
+                if(!productsPrices.ContainsKey(name))
+                {
+                    productsPrices[name] = price;
+                    productsQuantity[name] = quantity;
+                    productsFinalPrices[name] = price * quantity;
+                }
+                else
+                {
+                    productsPrices[name] = price;
+                    productsQuantity[name] += quantity;
+                    productsFinalPrices[name] = productsPrices[name] * productsQuantity[name];
+                    
+                }
+            }
+            foreach(var x in productsFinalPrices)
+            {
+                Console.WriteLine($"{x.Key} -> {x.Value:f2}");
+            }
+        }
+    }
+}
+
---
+++ 08. Associative Arrays /Exercise/06_Courses/06_Courses/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> coursesStudentsCount = new Dictionary<string, int>();
+            Dictionary<string, string> coursesStudents = new Dictionary<string, string>();
+            char[] chars = { ' ', ':', ' ' };
+            string command;
+            while((command=Console.ReadLine())!="end")
+            {
+                string[] segments = Console.ReadLine().Split(chars);
+                string courseName = segments[0];
+                string student = segments[1];
+                if(!coursesStudentsCount.ContainsKey(courseName))
+                {
+                    coursesStudentsCount[courseName]++;
+                    coursesStudents[courseName] = student; 
+                }
+                else
+                {
+                    coursesStudentsCount[courseName]++;
+                    coursesStudents[courseName] = student;
+                }
+            }
+            foreach(var x in coursesStudentsCount)
+            {
+                Console.WriteLine($"{x.Key}: {x.Value}");
+            }
+            foreach(var x in coursesStudents)
+            {
+                Console.WriteLine($"-- {x.Value}");
+            }
+        }
+    }
+}
+
