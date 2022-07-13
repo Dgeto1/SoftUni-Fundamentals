a3cbbc900e211de2368d5a25a9cfc8f49f83f0c5
Hristo Papanov <hristopapanov13@gmail.com>
2022-07-13 20:28:37 +0300

    13.07

---
+++ 09. Text Processing/04_TextFilter/04_TextFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04_TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banList = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach(var bannedWord in banList)
+            {
+                string replacemnet = new string('*', bannedWord.Length);
+                text = text.Replace(bannedWord, replacemnet);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
+
---
+++ 09. Text Processing/05_DigitsLettersAndOthers/05_DigitsLettersAndOthers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _05_DigitsLettersAndOthers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string st = Console.ReadLine();
+            string numbers = string.Empty;
+            string letters = string.Empty;
+            string chars = string.Empty;
+            for(int i=0; i<st.Length;i++)
+            {
+                if (char.IsDigit(st[i]))
+                {
+                    numbers += st[i];
+                }
+                else if (char.IsLetter(st[i]))
+                {
+                    letters += st[i];
+                }
+                else
+                {
+                    chars += st[i];
+                }
+            }
+            Console.WriteLine(numbers);
+            Console.WriteLine(letters);
+            Console.WriteLine(chars);
+        }
+    }
+}
+
